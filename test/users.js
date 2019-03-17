556e0906316482e31ad80ac5ea085231c9929216
Murat <Murat-305@hotmail.com>
2019-03-16 23:47:28 -0600

    Hmwk 3
    hmwk 3

--- test/users.js
+++ test/users.js
@@ -1,72 +1,43 @@
-//Require the dev-dependencies
-let envPath = __dirname + "/../.env"
-require('dotenv').config({path:envPath})
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../server');
-var User = require('../Users');
-let should = chai.should();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
-chai.use(chaiHttp);
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true } );
+mongoose.set('useCreateIndex', true);
 
 
-let login_details = {
-    'name': 'test',
-    'username': 'email@email.com',
-    'password': '123@abc'
-}
+var UserSchema = new Schema({
+    name: String,
+    username: { type: String, required: true, index: { unique: true }},
+    password: { type: String, required: true, select: false }
+});
 
-//Our parent block
-describe('Register, Login and check token', () => {
-    beforeEach((done) => { //Before each test we empty the database
-        //User.remove({ name: 'test' }, function(err, user) {
-        //    if (err) throw err;
-        //});
-        done();
-    });
 
-    after((done) => { //Before each test we empty the database
-        User.deleteOne({ name: 'test' }, function(err, user) {
-            if (err) throw err;
-        });
-        done();
-    });
-/*
-  * Test the /GET route
-  */
-describe('/signup ', () => {
-    it('it should Register, Login, and check token', (done) => {
-    chai.request(server)
-        .post('/signup')
-        .send(login_details)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.success.should.be.eql(true);
-            console.log('signup')
-            // follow up with login
-            chai.request(server)
-                .post('/signin')
-                .send(login_details)
-                .end((err, res) => {
-                    console.log('this was run the login part');
-                    res.should.have.status(200);
-                    res.body.should.have.property('token');
+UserSchema.pre('save', function(next) {
+    var user = this;
 
-                    let token = res.body.token;
-                    console.log(token);
-                    // follow up with requesting user protected page
-                    chai.request(server)
-                        .post('/postjwt')
-                        // we set the auth header with our token
-                        .set('Authorization', token)
-                        .send({ echo: '' })
-                        .end((err, res) => {
-                            res.should.have.status(200);
-                            res.body.should.have.property('echo');
-                            done(); // Don't forget the done callback to indicate we're done!
-                        })
-                });
-        });
+
+    if (!user.isModified('password')) return next();
+
+
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err);
+
+        // change the password to the hashed version
+        user.password = hash;
+        next();
     });
 });
-});
+
+UserSchema.methods.comparePassword = function(password, callback) {
+    var user = this;
+
+    bcrypt.compare(password, user.password, function(err, isMatch) {
+        callback(isMatch) ;
+    });
+};
+
+
+module.exports = mongoose.model('User', UserSchema);
